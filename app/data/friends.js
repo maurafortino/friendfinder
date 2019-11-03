11b054d44987ecbedb15858230caee135e49a4ae
maurafortino <maurafortino@gmail.com>
2019-11-03 18:36:17 -0500

    working app - have some minor fixes to make

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendArray = [
+    {
+    friendName: "Ron Stoppable",
+    friendPhoto: "https://static.comicvine.com/uploads/scale_small/5/51394/1817515-ron_stoppable.jpg",
+    friendScores: [
+        5,
+        3,
+        2,
+        1,
+        4,
+        5,
+        5,
+        3,
+        3,
+        2       
+    ]
+},
+{
+    friendName: "Shego",
+    friendPhoto: "https://static.comicvine.com/uploads/original/11128/111289205/5863734-main.png",
+    friendScores: [
+        2,
+        2,
+        2,
+        2,
+        2,
+        1,
+        1,
+        1,
+        5,
+        5       
+    ]
+}
+];
+
+module.exports = friendArray
