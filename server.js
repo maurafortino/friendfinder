a9ddab150c6b298e367ff89de8f2a3b5de8c6575
maurafortino <maurafortino@gmail.com>
2019-11-04 20:19:05 -0500

    edited app so that server can read the css file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname, './app/public')));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
